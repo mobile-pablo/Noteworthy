a8fd6323cb7981a6b5b6e19a9e2c0c7843a5bdb6
mobile-pablo <pawel.lukasz001@gmail.com>
2023-03-24 14:41:15 +0100

    Add DI modules

---
+++ app/src/main/java/com/mobile/pablo/iosnotes/di/NotesApplication.kt
@@ -0,0 +1,7 @@
+package com.mobile.pablo.iosnotes.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NotesApplication : Application()
---
+++ core/src/main/java/com/mobile/pablo/core/di/CoreModule.kt
@@ -0,0 +1,10 @@
+package com.mobile.pablo.core.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoreModule {
+}
