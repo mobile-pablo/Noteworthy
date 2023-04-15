0ec278bb7057c4b050542841efced666659e1032
mobile-pablo <pawel.lukasz001@gmail.com>
2023-04-15 17:07:07 +0200

    Config multidex

--- app/src/main/java/com/mobile/pablo/iosnotes/di/NotesApplication.kt
+++ app/src/main/java/com/mobile/pablo/iosnotes/di/NotesApplication.kt
@@ -1,7 +1,15 @@
 package com.mobile.pablo.iosnotes.di
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class NotesApplication : Application()
+class NotesApplication : Application() {
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
