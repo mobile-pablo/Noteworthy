1746a0064c1b06c0799eea06d401fa96f4e4100d
mobile-pablo <pawel.lukasz001@gmail.com>
2023-04-19 20:34:13 +0200

    Rollback

--- app/src/androidTest/java/com/mobile/pablo/iosnotes/tests/NoteTest.kt
+++ app/src/androidTest/java/com/mobile/pablo/iosnotes/tests/NoteTest.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.mobile.pablo.iosnotes.ext.isDisplayed
-import com.mobile.pablo.iosnotes.ext.assertions
 import com.mobile.pablo.iosnotes.screens.NoteScreen
 import com.mobile.pablo.note.mock.MOCK_NOTE_LIST
 import com.mobile.pablo.note.mock.MockNoteScreen
