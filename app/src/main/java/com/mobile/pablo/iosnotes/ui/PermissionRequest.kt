65b91574127ac539ac407954efa9d88b8be812a8
mobile-pablo <pawel.lukasz001@gmail.com>
2023-04-22 14:48:27 +0200

    Fix issue

--- app/src/main/java/com/mobile/pablo/iosnotes/ui/PermissionRequest.kt
+++ app/src/main/java/com/mobile/pablo/iosnotes/ui/PermissionRequest.kt
@@ -1,10 +1,14 @@
 package com.mobile.pablo.iosnotes.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.shouldShowRationale
@@ -14,13 +18,15 @@ import kotlinx.coroutines.launch
 @Composable
 fun PermissionRequest(permissionState: PermissionState) {
     val scope = rememberCoroutineScope()
-    Column {
-        val textToShow = if (permissionState.status.shouldShowRationale) {
-            "The camera is important for this app. Please grant the permission."
-        } else {
-            "Camera permission required for this feature to be available. " +
-                "Please grant the permission"
-        }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val textToShow =
+            if (permissionState.status.shouldShowRationale) "The camera is important for this app"
+            else "Camera permission required "
+
         Text(textToShow)
         Button(onClick = { scope.launch { permissionState.launchPermissionRequest() } }) {
             Text("Request permission")
