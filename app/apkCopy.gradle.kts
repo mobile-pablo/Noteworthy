51bd805a8dcd3edd7c17cf0043bdadd1521095a4
mobile-pablo <pawel.lukasz001@gmail.com>
2023-04-24 16:15:39 +0200

    Work on coping a apk files

---
+++ app/apkCopy.gradle.kts
@@ -0,0 +1,16 @@
+import org.gradle.api.tasks.Copy
+
+val copyApk by tasks.registering(Copy::class) {
+    from("build/outputs/apk/androidTest/debug")
+    into("jenkins-build/")
+    include("*.apk")
+}
+
+val copyUIApk by tasks.registering(Copy::class) {
+    from("build/outputs/apk/debug")
+    into("jenkins-build/")
+    include("*.apk")
+}
+
+tasks.getByName("build").finalizedBy(copyApk)
+tasks.getByName("build").finalizedBy(copyUIApk)
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
 apply(from = "../ktlint.gradle.kts")
+apply(from = "apkCopy.gradle.kts")
 
 plugins {
     id("com.android.application")
