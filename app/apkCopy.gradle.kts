0363aae67f008f0475c4b8e2fe122d5301aafeb4
mobile-pablo <pawel.lukasz001@gmail.com>
2023-04-24 16:50:37 +0200

    Work on coping a apk files

--- app/apkCopy.gradle.kts
+++ app/apkCopy.gradle.kts
@@ -1,15 +1,17 @@
 import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.JavaExec
 
-val copyApk by tasks.register<JavaExec>("copyApk") {
+val copyUIApk by tasks.register<JavaExec>("copyUIApk") {
     mainClass.set("CopyApkTask")
     classpath = files()
     args("build/outputs/apk/androidTest/debug", "jenkins-build", "*.apk")
 }
 
-val copyUIApk by tasks.register<JavaExec>("copyUIApk") {
+val copyApk by tasks.register<JavaExec>("copyApk") {
     mainClass.set("CopyApkTask")
     classpath = files()
     args("build/outputs/apk/debug", "jenkins-build", "*.apk")
 }
 
-tasks.getByName("build").finalizedBy(copyApk, copyUIApk)
+tasks.getByName("build").finalizedBy(copyApk)
+tasks.getByName("build").finalizedBy(copyUIApk)
